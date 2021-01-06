dcb8ee6d2c5289f76deb32ddf080020ed0bbc26c
Marc Steele <marcsteele2014@gmail.com>
2021-01-06 18:58:54 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,6 +4,7 @@ class Artist
 
   def initialize(name)
     @name = name
+<<<<<<< HEAD
   end
 
   def songs
@@ -21,6 +22,25 @@ class Artist
 
   def self.song_count
     Song.all.length
+=======
+    @songs = []
+  end
+
+  # def songs
+  #
+  # end
+
+  def add_song
+
+  end
+
+  def add_song_by_name(name)
+
+  end
+
+  def self.song_count
+
+>>>>>>> 80efa210000c8f9d9e1f3ccbfac123f2afdbc29e
   end
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -14,11 +14,18 @@ class Song
   end
 
   def artist_name
+<<<<<<< HEAD
     case self.artist
     when nil
       return nil
     else
       return self.artist.name
+=======
+    if self.artist = Artist
+      return self.artist.name
+    else
+      nil
+>>>>>>> 80efa210000c8f9d9e1f3ccbfac123f2afdbc29e
     end
   end
 
